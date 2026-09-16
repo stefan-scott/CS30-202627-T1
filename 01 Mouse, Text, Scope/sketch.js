9251da202c6c13c4c89ea30c90726caa5e9c2744
stefan-scott <scottst@spsd.sk.ca>
2026-09-16 14:10:03 -0600

    Demo Part Two

--- 01 Mouse, Text, Scope/sketch.js
+++ 01 Mouse, Text, Scope/sketch.js
@@ -4,6 +4,7 @@
 
 // Global Variables
 let x = 100;  let y = 100;
+let c = "green";
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,28 +12,57 @@ async function setup() {
 
 function draw() {
   background(220); 
-  textSize(40);    
+  mouseReport();  
+  updateSquare();
+}
 
+function updateSquare(){
+  //movement code here
+  //keyCode is LAST PRESSED CODE.
+  if(keyCode===40 && keyIsPressed){
+    y+=2;
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    x -= 5;
+  }
+  if(keyIsDown(RIGHT_ARROW)){
+    x += 5;
+  }
+  if(keyIsDown(UP_ARROW)){
+    y-=5;
+  }
+
+
+  //draw our character (a square) on screen
+  fill(c); 
+  square(x,y,50);
+}
+
+function mouseReport(){
+  textSize(40);    
   //mouse-related system variables (managed by library)
-  
+  fill("black");
   let mouseInfo = mouseIsPressed + " "
                   + mouseButton.left + " "
                   + mouseButton.center + " "
                   + mouseButton.right;
   text(mouseInfo, mouseX, mouseY);
-  
-  drawSquare();
 }
 
-function drawSquare(){
-  //draw our character (a square) on screen
-  square(x,y,50);
-}
+
 
 function keyPressed(){
   //this function calls automatically
   // - for single press captures
-  print(key + " " + keycode);
+  print(key + " " + keyCode);
+
+  if(key==="a"){
+    c = "red";
+  }
+  else if(keyCode===32){ //space
+    c = "yellow";
+  }
+  
 
   // for interest's sake → 
   // [ALT] - 2 to 4 digit number
